ec2228a0f847e83e46bc15fb92344db374e49de9
reqZ223 <u3241596@uni.canberra.edu.au>
2024-08-14 14:06:43 +1000

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import {Navbar, Footer} from './components';
+
+function App() {
+
+
+  return (
+    <>
+      <div className="App">
+        <Navbar />
+        
+        <Footer />
+      </div>
+    </>
+  )
+}
+
+export default App

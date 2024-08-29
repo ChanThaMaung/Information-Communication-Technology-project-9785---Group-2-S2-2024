253c6597b2b93a2b1ae08505ad625097663a66e2
[Haiomtg] <[haiomtg@gmail.com]>
2024-08-29 17:58:03 +1000

    bar chart

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,50 +1,61 @@
 // import * as React from 'react';
 
-import { PieChart } from '@mui/x-charts/PieChart';
+import { PieChart, BarChart } from '@mui/x-charts';
 
 export default function Dashboard() {
 
-    const createPieChat = () => {
-  return (
-    <PieChart
-      series={[
-        {
-          data: [
-            { id: 0, value: 10, label: 'series A' },
-            { id: 1, value: 15, label: 'series B' },
-            { id: 2, value: 20, label: 'series C' },
-          ],
-        },
-      ]}
-      width={400}
-      height={200}
-    />
-  );
-}
-
+  const createBarChart = () => {
     return (
-        <>
-            <div className="w-full">
-                <nav className="bg-slate-500 p-4">
-                    <div className="container mx-auto">
-                        <a href="#" className="text-3xl text-white font-semibold font-mono">Dashboard</a>
-                    </div>
-                </nav>
-                <div className="w-full grid grid-cols-3 gap-4">
-                    <div className="bg-red-700 h-8 col-span-2 h-full">
-                        {createPieChat()}
-                    </div>
-                    <div className="bg-slate-400 h-8">
+      <BarChart
+        xAxis={[{ scaleType: 'band', data: ['group A', 'group B', 'group C'] }]}
+        series={[{ data: [4, 3, 5] }, { data: [1, 6, 3] }, { data: [2, 5, 6] }]}
+        width={500}
+        height={300}
+      />
+    );
+  }
 
-                    </div>
-                    <div className="bg-amber-300 h-screen col-span-full">
+  const createPieChat = () => {
+    return (
+      <PieChart
+        series={[
+          {
+            data: [
+              { id: 0, value: 10, label: 'series A' },
+              { id: 1, value: 15, label: 'series B' },
+              { id: 2, value: 20, label: 'series C' },
+            ],
+          },
+        ]}
+        width={400}
+        height={200}
+      />
+    );
+  }
 
-                    </div>
+  return (
+    <>
+      <div className="w-full">
+        <nav className="bg-slate-500 p-4">
+          <div className="container mx-auto">
+            <a href="#" className="text-3xl text-white font-semibold font-mono">Dashboard</a>
+          </div>
+        </nav>
+        <div className="w-full grid grid-cols-3 gap-4">
+          <div className="bg-red-700 text-center col-span-2 h-full">
+            {createPieChat()}
+          </div>
+          <div className="bg-slate-400 h-full">
+            {createBarChart()}
+          </div>
+          <div className="bg-amber-300 h-screen col-span-full">
+            
+          </div>
 
-                </div>
-            </div>
-        </>
-    )
+        </div>
+      </div>
+    </>
+  )
 }
 
 

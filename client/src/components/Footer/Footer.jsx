db4ee221ba984b07c678fbffb41eccd33daa8b3a
[Haiomtg] <[haiomtg@gmail.com]>
2024-09-18 18:18:05 +1000

    s

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,29 +1,27 @@
 function Footer() {
     return (
-        <div className="pt-16 mt-16 border-t border-gray-200">
+        <div className="pt-8 pb-10 mt-16 border-t border-gray-200">
             <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
                 <div className="text-center md:text-left">
-                    <img className="mb-4 mx-auto md:mx-0" src="https://getbootstrap.com/docs/4.0/assets/brand/bootstrap-solid.svg" alt="" width="24" height="24" />
+                    <i className="fa-solid fa-coins text-indigo-600 text-2xl"></i>
                     <small className="block text-gray-500">© 2017-2018</small>
                 </div>
                 <div>
                     <h5 className="font-semibold">Features</h5>
                     <ul className="space-y-2">
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Cool stuff</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Random feature</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Team feature</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Stuff for developers</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Another one</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Last time</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Trading and Exchange Platform</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Cryptocurrency Wallet Integration</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Customer Support</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Educational Resources</a></li>
                     </ul>
                 </div>
                 <div>
                     <h5 className="font-semibold">Resources</h5>
                     <ul className="space-y-2">
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Resource</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Resource name</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Another resource</a></li>
-                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Final resource</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Educational Content</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">API Documentation</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Tax Resources</a></li>
+                        <li><a className="text-gray-500 hover:text-gray-700" href="#">Social Media Integration</a></li>
                     </ul>
                 </div>
                 <div>
